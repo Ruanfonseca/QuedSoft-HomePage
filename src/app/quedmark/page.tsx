1d67847a62b02ffb4560949543266c8c40a9e7d3
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2024-10-25 16:50:03 -0300

    alteração na página da quedmark

--- src/app/quedmark/page.tsx
+++ src/app/quedmark/page.tsx
@@ -16,19 +16,35 @@ const QuedMark = () => {
     <>
       <Navbar />
       <div className="quedMarkContainer">
+        
         <div className="quedMarkLogo">
           <Image className="logoquedImg" src={QuedMarkLogo2} alt="Logo QuedMark" />
         </div>
+
         <div className="quedMarkDescricao">
-          <h1 className="titulo">Escolha um plano para você!</h1>
-        </div>
+              <h1 className="titulo">Sobre</h1>
+              <p className='textosobre'>
+                A QuedMark nasceu para ajudar salões de beleza , studio de estética, barbearias e profissionais autônomos a ter mais tranquilidade na hora de organizar seus atendimentos. Com uma plataforma simples e prática, você consegue marcar, gerenciar e ajustar seus compromissos de forma eficiente, tudo em um só lugar. Esqueça a dor de cabeça com desencontros de horários e atrasos: com a QuedMark, você ganha controle total sobre sua agenda e pode focar no que realmente importa, que é atender bem os seus clientes. 
+
+                Hoje em dia, com a correria do dia a dia, é fundamental contar com uma ferramenta que simplifique o agendamento de horários e facilite a comunicação com os clientes. A QuedMark cuida disso para você, enviando lembretes automáticos, permitindo o acesso fácil ao histórico de atendimentos e trazendo relatórios para você acompanhar o desempenho do seu negócio. 
+
+                Afinal, tempo é dinheiro, e a QuedMark está aqui para ajudar você a economizar os dois, deixando a gestão dos atendimentos mais leve e organizada.
+              </p>
+        
+        
+        
+              <h1 className="titulo">Conheça nossos planos </h1>
+            </div>
+
+
 
         <div className="cardContainer">
           <div className="card">
             <div className="card-title">Plano Bronze</div>
             <div className="descricao">
               <p>
-                <span>R$ 59,90</span>/licença completa/mês<br/>
+                <span>R$ 59,90</span><br/>
+                 Licença completa/mês<br/>
                  40 agendamentos  
               </p>
             </div>
@@ -40,7 +56,8 @@ const QuedMark = () => {
             <div className="card-title">Plano Prata</div>
             <div className="descricao">
               <p>
-                <span>R$ 79,90</span>/licença completa/mês<br/>
+                <span>R$ 79,90</span><br/>
+                Licença completa/mês<br/>
                   80 agendamentos  
               </p>
             </div>
@@ -52,8 +69,9 @@ const QuedMark = () => {
             <div className="card-title">Plano Gold</div>
             <div className="descricao">
               <p>
-                <span>R$ 119,90</span>/licença completa/mês<br/>
-                  Números de agendamentos ilimitado 
+                <span>R$ 119,90</span><br/>
+                 Licença completa/mês<br/>
+                 Números de agendamentos ilimitado 
               </p>
             </div>
             <div className="botao-selecao">Escolher Plano</div>
